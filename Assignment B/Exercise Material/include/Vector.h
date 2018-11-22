99d4cc2dc8561cb3a5249364fac55ace2cd4d929
Maxi Rohleder <maxi.rohleder@fau.de>
2018-11-22 19:02:23 +0100

    inverse diagonal function

--- Assignment B/Exercise Material/include/Vector.h
+++ Assignment B/Exercise Material/include/Vector.h
@@ -1,29 +1,28 @@
-// this file is just for documenting the required interface to compile with MatrixTest.cpp (declarations only)
+// this file is just for documenting the required interface to compile with VectorTest.cpp (declarations only)
 #include <iostream>
 #include <assert.h>
-#include "Vector.h"
 
 using namespace std;
 
 template <typename T>
-class Matrix {
+class Vector {
 private:
 	size_t no_rows;
 	size_t no_cols;
 	T *data = nullptr;
 public:
 	// normal constructor
-	Matrix(size_t rows, size_t cols, T initValue) : no_cols(cols), no_rows(rows), data(new T[cols*rows]) {
+	Vector(size_t rows, size_t cols, T initValue) : no_cols(cols), no_rows(rows), data(new T[cols*rows]) {
 		for (size_t i = 0; i < no_rows*no_cols; i++) data[i] = initValue;
 	};
 	// destructor
-	~Matrix() { delete[] data; };
+	~Vector() { delete[] data; };
 	// copy constructor
-	Matrix (const Matrix<T>& n) : no_cols(n.no_cols), no_rows(n.no_rows), data(new T[n.no_cols*n.no_rows]) {
+	Vector(const Vector& n) : no_cols(n.no_cols), no_rows(n.no_rows), data(new T[n.no_cols*n.no_rows]) {
 		for (size_t i = 0; i < no_rows*no_cols; i++) data[i] = n.data[i];
 	};
 	// assignment just calls copy constructor
-	Matrix<T>& operator=(const Matrix<T>& n) {
+	Vector& operator=(const Vector& n) {
 		if (*this == n) return *this;
 		delete[] data;
 		no_cols = n.no_cols;
@@ -33,7 +32,7 @@ public:
 		return *this;
 	};
 	// value-setter i is the ith row and ranges between 0 and no_rows(excl)
-	T& operator()(size_t i, size_t j) {
+	double& operator()(size_t i, size_t j) {
 		assert(i >= 0 && i < no_rows && j >= 0 && j < no_cols);
 		return data[i*no_cols + j];
 	};
@@ -43,42 +42,43 @@ public:
 		return data[i*no_cols + j];
 	};
 	// elementwise comparison
-	bool operator ==(const Matrix<T>& n) const {
+	bool operator ==(const Vector& n) const {
 		if (no_rows != n.no_rows || no_cols != n.no_cols) return false;
 		for (size_t i = 0; i < no_cols*no_rows; i++) if (data[i] != n.data[i]) return false;
 		return true;
 	};
 	// elementwise negative comparison
-	bool operator !=(const Matrix<T>& n) const { return !(*this == n); };
+	bool operator !=(const Vector& n) const { return !(*this == n); };
 
 	// arithmetic operators
 	// element wise addition
-	Matrix<T>& operator +=(const Matrix<T>& n) {
+	Vector& operator +=(const Vector& n) {
 		assert(no_rows == n.no_rows && no_cols == n.no_cols);
 		for (size_t i = 0; i < no_cols*no_rows; i++) data[i] += n.data[i];
 		return *this;
 	};
 
-	Matrix<T> operator +(const Matrix<T>& n) const {
+	Vector operator +(const Vector& n) const {
 		assert(no_rows == n.no_rows && no_cols == n.no_cols);
-		Matrix ret = *this;
+		Vector ret = *this;
 		ret += n;
 		return ret;
 	};
 	// elementwise subtraction
-	Matrix<T>& operator -=(const Matrix<T>& n) {
+	Vector& operator -=(const Vector& n) {
 		assert(no_rows == n.no_rows && no_cols == n.no_cols);
 		for (size_t i = 0; i < no_cols*no_rows; i++) data[i] -= n.data[i];
 		return *this;
 	};
 
-	Matrix<T> operator -(const Matrix<T>& n) const {
+	Vector operator -(const Vector& n) const {
 		assert(no_rows == n.no_rows && no_cols == n.no_cols);
-		Matrix<T> ret = *this;
+		Vector ret = *this;
 		ret -= n;
 		return ret;
 	};
-	// matrix multiplication (pythonic matmul-symbol @ not overloadable)
+
+	// Vector multiplication (pythonic matmul-symbol @ not overloadable)
 	/*
 	a row has no_cols entries.
 	a col has no_rows entries.
@@ -87,9 +87,9 @@ public:
 	(s1, s2) @ (s2, s3) --> (s1, s3)
 	(no_rows, no_cols) @ (n.no_rows, n.no_cols) --> (no_rows, n.no_cols)
 	*/
-	Matrix<T> operator *(const Matrix<T>& n) const {
+	Vector operator *(const Vector& n) const {
 		assert(no_cols == n.no_rows);
-		Matrix<T> ret = Matrix<T>(no_rows, n.no_cols, (T) 0);
+		Vector<T> ret = Vector<T>(no_rows, n.no_cols, (T)0);
 		size_t s1 = no_rows;
 		size_t s2 = no_cols;
 		size_t s3 = n.no_cols;
@@ -107,40 +107,16 @@ public:
 		return ret;
 	};
 
-	Matrix<T>& operator *=(const Matrix<T>& n) {
+	Vector& operator *=(const Vector& n) {
 		*this = *this * n;
 		return *this;
 	};
 
-	// TODO implement
-	Vector<T> operator* (const Vector<T> & o) const {
-		
-	};
-
-	// TODO implement
-	Matrix<T> inverseDiagonal(const Matrix<T>& n) const {
-		Matrix<T> diag = n;
-		for (size_t c = 0; c < n.cols; c++)
-		{
-			for (size_t r = 0; r < n.rows; r++)
-			{
-				if (r != c) {
-					diag(r, c) = (T) 0.;
-				}
-				else {
-					diag(r, c) = 1 / n(r, c);
-				}
-			}
-		}
-		return diag;
-	};
-
-
 
 	// toString method
-	friend ostream& operator <<(ostream&, const Matrix&);
+	friend ostream& operator <<(ostream&, const Vector&);
 	// readin method
-	friend istream& operator >>(istream&, const Matrix&);
+	friend istream& operator >>(istream&, const Vector&);
 
 	// getter for private members
 	size_t rows() const { return no_rows; };
@@ -148,7 +124,7 @@ public:
 
 };
 
-ostream& operator <<(ostream& out, const Matrix& n) {
+ostream& operator <<(ostream& out, const Vector& n) {
 	for (size_t i = 0; i < n.no_cols*n.no_rows; i++)
 	{
 		if (i % n.no_cols == n.no_cols - 1) out << n.data[i] << endl << endl;
@@ -157,7 +133,7 @@ ostream& operator <<(ostream& out, const Matrix& n) {
 	return out;
 };
 
-istream& operator >>(istream& in, const Matrix& n) {
+istream& operator >>(istream& in, const Vector& n) {
 	for (size_t i = 0; i < n.no_cols*n.no_rows; i++)
 	{
 		in >> n.data[i];
