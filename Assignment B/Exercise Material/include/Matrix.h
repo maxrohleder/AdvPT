f3c16da073e411aab2aadc76f200ce49fdd7421e
Maxi Rohleder <maxi.rohleder@fau.de>
2018-11-25 23:05:30 +0100

    ....

--- Assignment B/Exercise Material/include/Matrix.h
+++ Assignment B/Exercise Material/include/Matrix.h
@@ -38,13 +38,19 @@ public:
 		return data[i*no_cols + j];
 	};
 	// elementwise comparison
-	bool operator ==(const Matrix<T, no_rows, no_cols>& n) const {
-		//if (no_rows != n.no_rows || no_cols != n.no_cols) return false; typechecking does that for us
-		for (size_t i = 0; i < no_cols*no_rows; i++) if (data[i] != n.data[i]) return false;
+	template<size_t r, size_t c>
+	bool operator ==(const Matrix<T, r, c>& n) const {
+		if (no_rows != r || no_cols != c) return false;
+		for (size_t i = 0; i < no_rows; ++i) {
+			for (size_t j = 0; j < no_cols; ++j) {
+				if (data[i*no_cols + j] != n(i, j)) return false;
+			}
+		}
 		return true;
 	};
 	// elementwise negative comparison
-	bool operator !=(const Matrix<T, no_rows, no_cols>& n) const { return !(*this == n); };
+	template<size_t r, size_t c>
+	bool operator !=(const Matrix<T, r, c>& n) const { return !(*this == n); };
 
 	// arithmetic operators
 	// element wise addition
@@ -82,20 +88,19 @@ public:
 	(s1, s2) @ (s2, s3) --> (s1, s3)
 	(no_rows, no_cols) @ (n.no_rows, n.no_cols) --> (no_rows, n.no_cols)
 	*/
-	Matrix<T, no_rows, no_cols> operator *(const Matrix<T, no_rows, no_cols>& n) const {
-		assert(no_cols == no_rows);
+	template<size_t r, size_t c>
+	Matrix<T, no_rows, c> operator *(const Matrix<T, r, c>& n) const {
+		// könnte static sein
+		assert(no_cols == r);
 		//assert
-		Matrix<T, no_rows, no_cols> ret = Matrix<T, no_rows, no_cols>((T) 0);
-		size_t s1 = no_rows;
-		size_t s2 = no_cols;
-		size_t s3 = no_cols;
+		Matrix<T, no_rows, c> ret(0);
 		// run along s1 and s3 and for each datapoint at (s1_r, s3_r) calc weighted sum 
-		for (size_t s1_r = 0; s1_r < s1; s1_r++) {
-			for (size_t s3_r = 0; s3_r < s3; s3_r++) {
+		for (size_t s1_r = 0; s1_r < no_rows; s1_r++) {
+			for (size_t s3_r = 0; s3_r < c; s3_r++) {
 				T datapoint = 0;
 				// loop over s1_rth row of this and s3_rth column of n and calculate weighted sum
-				for (size_t i = 0; i < s2; i++) {
-					datapoint += data[(s1_r*no_cols) + i] * n.data[(i*no_cols) + s3_r];
+				for (size_t i = 0; i < no_cols; i++) {
+					datapoint += data[(s1_r*no_cols) + i] * n(i, s3_r);
 				}
 				ret(s1_r, s3_r) = datapoint;
 			}
